7e7d7c2b3494d7e009c133f0fcec56f095f4a6ef
fouss98 <camarafousseinou6@gmail.com>
2023-01-05 14:25:53 +0000

    premier comit

---
+++ src/main/java/sn/isi/aminservice/AminServiceApplication.java
@@ -0,0 +1,13 @@
+package sn.isi.aminservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AminServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AminServiceApplication.class, args);
+	}
+
+}
